1e503a1cb9ff2e55c93681866f95792798de3d1e
Pedro França <pfranca.neto@gmail.com>
2025-06-08 17:39:07 -0300

    chamada do globalstyles no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyles } from './styles/global';
+
 export function App() {
-  return <h1>Hello</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>Hello</h1>;
+    </ThemeProvider>
+  );
 }
