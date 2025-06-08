486d0248ef8d2076b0e7ffbb9ffbbdcde26ef46e
Pedro França <pfranca.neto@gmail.com>
2025-06-08 20:06:41 -0300

    chamando componentes no App + logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyles } from './styles/global';
+import { Transactions } from './pages/Transections';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>Hello</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }
