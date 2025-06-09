b861b00d9630077bc526be3035e80b2cbbe82037
Pedro França <pfranca.neto@gmail.com>
2025-06-08 21:39:54 -0300

    iniciando table + ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyles } from './styles/global';
-import { Transactions } from './pages/Transections';
+import { Transactions } from './pages/Transactions';
 
 export function App() {
   return (
