275c92fafa027f9833b66a81357d0dfa2cd11bf9
Pedro França <pfranca.neto@gmail.com>
2025-06-11 22:34:19 -0300

    criando uma pasta Hooks com o useSummary abstraindo os calculos

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,31 +4,13 @@ import {
   CurrencyDollarIcon,
 } from '@phosphor-icons/react';
 
-import { SummaryCard, SummaryContainer } from './styles';
-import { useTransactions } from '../../contexts/TransactionsContext';
 import { priceFormatter } from '../../utils/formatter';
+import { useSummary } from '../../hooks/useSummary';
+
+import { SummaryCard, SummaryContainer } from './styles';
 
 export function Summary() {
-  const { transactions } = useTransactions();
-
-  const summaryTotals = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const summaryTotals = useSummary();
 
   return (
     <SummaryContainer>
