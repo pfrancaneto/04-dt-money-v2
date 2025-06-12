2e9aca39e50adf340f214ff35ddb0b8b4e812869
Pedro França <pfranca.neto@gmail.com>
2025-06-12 17:47:51 -0300

    passando useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,26 +1,29 @@
+import { useMemo } from 'react';
 import { useTransactions } from '../contexts/TransactionsContext';
 
 export function useSummary() {
   const { transactions } = useTransactions();
 
-  const summaryTotals = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
+  const summaryTotals = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price;
+          acc.total += transaction.price;
+        } else {
+          acc.outcome += transaction.price;
+          acc.total -= transaction.price;
+        }
 
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+  }, []);
 
   return summaryTotals;
 }
